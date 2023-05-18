1c1267def7aec780fe2a64b86c4dc1942d18d96f
Jules <julesthuys@gmail.com>
2023-05-19 00:01:45 +0200

    Append

--- sources/exec/exec_all.c
+++ sources/exec/exec_all.c
@@ -6,7 +6,7 @@
 /*   By: jthuysba <jthuysba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 18:01:28 by jthuysba          #+#    #+#             */
-/*   Updated: 2023/05/18 16:20:44 by jthuysba         ###   ########.fr       */
+/*   Updated: 2023/05/18 23:50:49 by jthuysba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	exec_type(t_cmd *cmd, t_exec *data)
 		exec_cmd(cmd, data);
 	else if (cmd->type == STDIN)
 		exec_stdin(cmd, data);
-	else if (cmd->type == STDOUT)
+	else if (cmd->type == STDOUT || cmd->type == APPEND)
 		exec_stdout(cmd, data);
 	return (0);
 }
