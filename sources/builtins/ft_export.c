ce756603aa3224c3be85f4bbeb75ba38849ad3b1
Tasnim MEJRI <tmejri@e1r3p13.clusters.42paris.fr>
2023-05-19 15:32:32 +0200

    add builtin

--- sources/builtins/ft_export.c
+++ sources/builtins/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: tmejri <tmejri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 19:11:23 by tas               #+#    #+#             */
-/*   Updated: 2023/05/16 17:40:36 by tmejri           ###   ########.fr       */
+/*   Updated: 2023/05/19 15:26:17 by tmejri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,6 @@
 #include "heredoc.h"
 
 t_list	**list_ENVI;
-
-/*TO DO
-- export simple return la list par ordre aasci
-- export a ---> a='' mais e s'affiche qu'avec export
-- si export une var qui existe déja ça la modifie
-//MODIFIER LA VAR SI ELLE EXISTE
-*/
 
 int check_equal(char *str)
 {
@@ -63,71 +56,135 @@ int is_in_env(char *str)
     return (0);
 }
 
-/* swap 2 elts d'une liste */
-// void	ft_swap(t_list *first, t_list *second)
-// {
-//     t_list  *tmp;
+t_list  **ft_copy_list(t_list  **copy)
+{
+    while (*list_ENVI)
+    {
+        add_list(copy, (*list_ENVI)->content, 0);
+        (*list_ENVI) = (*list_ENVI)->next;
+    }
+    return (copy);
+}
 
-//     tmp = first;
-//     first = second;
-//     second = tmp;
-// }
+/* return taill de str jusau'a = */
+int	ft_strlen_var(char *str)
+{
+	int	i;
 
-// t_list  **ft_copy_list(t_list  **copy)
-// {
-//     while (*list_ENVI)
-//     {
-//         add_list(copy, (*list_ENVI)->content);
-//         (*list_ENVI) = (*list_ENVI)->next;
-//     }
-//     return (copy);
-// }
+	i = 0;
+	while (str[i] && i != '=')
+		i++;
+	return (i);
+}
 
-// int is_sorted(t_list **list)
-// {
-//     while ((*list)->next != NULL)
-//     {
-//         if (ft_strncmp((*list)->content, (*list)->next->content, ft_strlen((*list)->content)) > 0)
-//             return (1);
-//         (*list) = (*list)->next;
-//     }
-//     return (0);    
-// }
+int is_sorted(t_list **list)
+{
+    t_list  *tmp;
+    int     min;
+    int     max;
 
-// int sort_env(t_list **list)
-// {
-//     t_list  *tmp;
+    (void)min;
+    (void)max;
     
-//     tmp = *list;
-//     while (is_sorted(list) == 1)
-//     {
-//         *list = tmp;
-//         if (ft_strncmp((*list)->content, (*list)->next->content, ft_strlen((*list)->content)) > 0)
-//             ft_swap
-//     }
-// }
+    min = 0;
+    max = 0;
+    tmp = *list;
+    while ((*list)->next != NULL)
+    {
+        if (ft_strlen_var((*list)->content) >= ft_strlen_var((*list)->next->content))
+        {
+            min = ft_strlen_var((*list)->next->content);
+            max = ft_strlen_var((*list)->content);
+        }
+        else
+        {
+            min = ft_strlen_var((*list)->content);
+            max = ft_strlen_var((*list)->next->content);
+        }
+        if (ft_strncmp((*list)->content, (*list)->next->content, min) > 0)
+            return (1);
+        (*list) = (*list)->next;
+    }
+    *list = tmp;
+    return (0);    
+}
 
-// int sort_env_ascii(void)
-// {
-//     t_list  **copy;
+t_list  **sort_env(t_list **list)
+{
+    t_list  *tmp;
+    int     min;
+    int     max;
 
-//     copy = malloc(sizeof(t_list));
-//     ft_memset(copy, 0, sizeof(t_list));
-//     copy = ft_copy_list(copy);
-//     print_list(copy);
-//     return (0);
-// }
+    char  *tmp1;
+    char  *tmp2;
+
+    (void)min;
+    (void)max;
+    
+    min = 0;
+    max = 0;
+    tmp = *list;
+    while (is_sorted(list) == 1)
+    {
+        if (ft_strlen_var((*list)->content) >= ft_strlen_var((*list)->next->content))
+        {
+            min = ft_strlen_var((*list)->next->content);
+            max = ft_strlen_var((*list)->content);
+        }
+        else
+        {
+            min = ft_strlen_var((*list)->content);
+            max = ft_strlen_var((*list)->next->content);
+        }
+        if (ft_strncmp((*list)->content, (*list)->next->content, min) > 0)
+        {
+            tmp1 = ft_strdup_size((*list)->content, ft_strlen((*list)->content));
+            tmp2 = ft_strdup_size((*list)->next->content, ft_strlen((*list)->next->content));
+            (*list)->content = ft_strdup_size(tmp2, ft_strlen(tmp2));
+            (*list)->next->content = ft_strdup_size(tmp1, ft_strlen(tmp1));
+            free(tmp1);
+            free(tmp2);
+            *list = tmp;
+        }
+        (*list) = (*list)->next;
+    }
+    *list = tmp;
+    return (list);
+}
+
+void	print_export(t_list **list)
+{
+	t_list *tmp;
+
+	tmp = (*list);
+	while (*list)
+	{
+		printf("export %s\n", (*list)->content);
+		(*list) = (*list)->next;
+	}
+	(*list) = tmp;
+}
+
+int sort_env_ascii(void)
+{
+    t_list  **copy;
+
+    copy = malloc(sizeof(t_list));
+    ft_memset(copy, 0, sizeof(t_list));
+    copy = ft_copy_list(copy);
+    copy = sort_env(copy);
+    print_export(copy);
+    return (0);
+}
 
 int ft_export(t_list **list_token)
 {
     t_list  *tmp;
+    char    *add_env;
     
     tmp = *list_ENVI;
-    if (ft_lstsize(*list_token)== 1)
-    {
-        printf("en cours de construction\n");
-        // sort_env_ascii();
-    }
+    if (ft_lstsize(*list_token) == 1)
+        sort_env_ascii();
     else
     {
         while (*list_token != NULL)
@@ -136,7 +193,18 @@ int ft_export(t_list **list_token)
             if (check_equal((*list_token)->content) == 1)
             {
                 if (is_in_env(take_off_equal((*list_token)->content)) == 0)
-                    add_list(list_ENVI, (*list_token)->content);
+                {
+                    add_env = ft_strdup_size((*list_token)->content, ft_strlen((*list_token)->content));
+                    while ((*list_token)->next != NULL && (*list_token)->flag_space != 1)
+                    {
+                        printf("content: %s, %d\n", (*list_token)->content, (*list_token)->flag_space);
+                        (*list_token) = (*list_token)->next;
+                        add_env = ft_strjoin_mod(add_env, (*list_token)->content, 0);
+                    }
+                    add_list(list_ENVI, add_env, 0);
+                    *list_ENVI = tmp;
+                    return (0);
+                }
                 else
                 {
                     while (*list_ENVI)
@@ -145,8 +213,14 @@ int ft_export(t_list **list_token)
                             (*list_ENVI) = (*list_ENVI)->next;
                         else if (ft_strcmp(take_off_equal((*list_ENVI)->content), take_off_equal((*list_token)->content)) == 0)
                         {
-                            (*list_ENVI)->content = ft_strjoin_mod("", (*list_token)->content, 0);
-                            break;
+                            (*list_ENVI)->content = ft_strdup_size((*list_token)->content, ft_strlen((*list_token)->content));
+                            while ((*list_token)->next != NULL && (*list_token)->next->flag_space != 1)
+                            {
+                                (*list_token) = (*list_token)->next;
+                                (*list_ENVI)->content = ft_strjoin_mod((*list_ENVI)->content, (*list_token)->content, 0);
+                            }
+                            *list_ENVI = tmp;
+                            return (0);
                         }
                     }
                 }
