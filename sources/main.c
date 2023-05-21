7e6a65715f8c9c15b3cdd2ba28778d60606809a8
Jules <julesthuys@gmail.com>
2023-05-21 20:25:50 +0200

    Commented del empty token

--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tas <tas@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: jthuysba <jthuysba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 19:39:21 by tas               #+#    #+#             */
-/*   Updated: 2023/05/20 00:14:49 by tas              ###   ########.fr       */
+/*   Updated: 2023/05/21 20:24:36 by jthuysba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@ void    init_list(t_list **list_token, char *input)
     substitute_dollar(list_token);
     get_type(list_token);
     remove_list_quotes(list_token);
-    del_empty_token(list_token);
-    print_list(list_token);
+    printf("HERE\n");
+    // del_empty_token(list_token);
+    // print_list(list_token);
 }
 
 int main(int argc, char **argv, char **env)
@@ -32,19 +33,19 @@ int main(int argc, char **argv, char **env)
     char *input;
     t_list **list_token;
     list_ENVI = get_list_env(env);
-    while(1)
+    while (1)
     {
         signal(SIGQUIT, SIG_IGN);
         // signal(SIGINT, &ctrl_c);
         input = get_input();
         list_token = malloc(sizeof(t_list));
         list_token[0] = NULL;
+        
         init_list(list_token, input);
         syntax_error(list_token);
 
-exec_builtin(list_token);
-        
-        // exec_all(*list_token, env);
+        // exec_builtin(list_token);
+        exec_all(*list_token, env);
     }
     return (0);
 }
