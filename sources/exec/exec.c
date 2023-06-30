a01efd2bace92f3859a6478d8b8f5ea97be49b98
Jules THUYSBAERT <jthuysba@e1r2p15.clusters.42paris.fr>
2023-06-30 12:02:54 +0200

    hdoc malloc fixs

--- sources/exec/exec.c
+++ sources/exec/exec.c
@@ -106,7 +106,11 @@ int	we_exec(t_minishell *data)
 	if (prep_cmd(data) == 1)
 		return (destroy_exec(data->x), 1);
 	open_pipes(data);
-	exec_heredoc(data);
+	if (exec_heredoc(data) != 0)
+	{
+		data->code_err = 1;
+		return (destroy_exec(data->x), 1);
+	}
 	if (go_exec(data->x, data) != 0)
 	{
 		data->code_err = 1;
